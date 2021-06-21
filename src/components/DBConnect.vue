ccdc236989d19c66a0049bb325f6e2c38bbcf54e
Raaghav Agarwal <Raaghav.Agarwal@gmail.com>
2021-06-21 13:02:39 -0600

    got axios to work

---
+++ src/components/DBConnect.vue
@@ -0,0 +1,44 @@
+<template>
+   <button @click="connect()">Connect to Database</button>
+   <p> {{ info }} </p>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      info: null
+    }
+  },
+
+  methods:{
+    connect(){
+      console.log("Button Pressed")
+      axios
+        .get('http://localhost:8000/README.md', {
+          headers: {"Access-Control-Allow-Origin": "*"}
+        })
+        .then(response => (this.info = response))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
